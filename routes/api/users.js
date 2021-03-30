7aa89a8f6819694c5962732efddf37b35208d81d
Irina Mai <irinam3340@gmail.com>
2021-03-30 14:21:58 +0300

    additional tasks added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { current } = require('../../controllers/users')
+const { current, updateUser } = require('../../controllers/users')
 const isLogged = require('../../config/passport')
 
 router.get('/current', isLogged, current)
+router.patch('/', isLogged, updateUser)
 
 module.exports = router
